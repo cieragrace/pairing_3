92f6059aee60e70daa98f729fd05b44ce3191c6f
Ciera DePauw <cieragrace18@gmail.com>
2022-09-22 09:55:24 -0600

    adjust formatting

--- object_literals/object_practice.js
+++ object_literals/object_practice.js
@@ -1,7 +1,10 @@
 // virtual address book
 var contact1 = {
   name: "Mark Whalburg",
-  number: ["970-546-9345"],
+  number: "970-546-9345",
   company: "Show Biz"
   activeContact: true
+console.log("Name", + "number", + "company");
+
+  }
 }
