c2e0e1aee5cc2da74dd1eab49ee16a2685a7d8c6
Ciera DePauw <cieragrace18@gmail.com>
2022-09-22 10:29:17 -0600

    add second contact

--- object_literals/object_practice.js
+++ object_literals/object_practice.js
@@ -3,5 +3,11 @@ var contact1 = {
   name: "Mark Whalburg",
   number: "970-546-9345",
   company: "Show Biz"
+
+var contact2 = {
+  name: "Mom",
+  number: "555-555-5555"
+  company: "Real Estate"
+}
   }
-console.log(contact1);
+console.log(contact1 + contact2);
