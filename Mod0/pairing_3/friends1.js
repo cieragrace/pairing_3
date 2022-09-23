f9e126f2ec31dfb2733ce106a3062007cf61f8b0
Ciera DePauw <cieragrace18@gmail.com>
2022-09-23 12:46:26 -0600

    make changes to our array

--- Mod0/pairing_3/friends1.js
+++ Mod0/pairing_3/friends1.js
@@ -2,3 +2,7 @@ var arrays = ["Tom", "Bob", "John", "Pam", "Amy"];
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var placesToVisit = ["Oregon", "California", "Montana", "Washington", "Idaho"];
+placesToVisit.pop();
+console.log(placesToVisit);
